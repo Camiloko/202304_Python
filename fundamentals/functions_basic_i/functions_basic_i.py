c406055c8299eb27930814c6bee45464c8b1776d
Camilo Flores <camilocifz36@gmail.com>
2023-05-17 15:39:45 -0400

    funciones_basicas

--- fundamentals/functions_basic_i/functions_basic_i.py
+++ fundamentals/functions_basic_i/functions_basic_i.py
@@ -1,46 +1,50 @@
 #1
 def number_of_food_groups():
     return 5
-print(number_of_food_groups())
+print(number_of_food_groups())  #tetornara el numero 5
 
 
 #2
 def number_of_military_branches():
     return 5
-#print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
-
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+#representara un error pues no esta definida la primera funcion del print
 
 #3
 def number_of_books_on_hold():
     return 5
     return 10
-print(number_of_books_on_hold())
+print(number_of_books_on_hold()) #retornara 5 y luego 10
+
+#al parecer retorno 5 y el 10 no, porque se detuvo en el retorno 5
 
 
 #4
 def number_of_fingers():
     return 5
     print(10)
-print(number_of_fingers())
+print(number_of_fingers())  #retonara 5 e imprimira el numero 10
+
+#nuevamente se detuvo en el return 5
 
 
 #5
 def number_of_great_lakes():
     print(5)
 x = number_of_great_lakes()
-print(x)
+print(x)    #imprimira el numero 5
 
 
 #6
 def add(b,c):
     print(b+c)
-print(add(1,2) + add(2,3))
+print(add(1,2) + add(2,3))  #imrpimira la suma de todos los numeros , que seria 8
 
 
 #7
 def concatenate(b,c):
     return str(b)+str(c)
-print(concatenate(2,5))
+print(concatenate(2,5)) #pondra los numeros uno al lado del otro, dando 25
 
 
 #8
@@ -52,7 +56,7 @@ def number_of_oceans_or_fingers_or_continents():
     else:
         return 10
     return 7
-print(number_of_oceans_or_fingers_or_continents())
+print(number_of_oceans_or_fingers_or_continents()) #retornara 10
 
 
 #9
@@ -62,16 +66,17 @@ def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
     else:
         return 14
     return 3
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))  #retornara 7
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))  #retornara 14
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
 
+#retornara 7 + 14, es decir 21
 
 #10
 def addition(b,c):
     return b+c
     return 10
-print(addition(3,5))
+print(addition(3,5))    #retonara 8
 
 
 #11
@@ -99,6 +104,7 @@ print(b)
 foobar()
 print(b)
 
+#imprimira 1 vez 500, y denuevo 500 luego 300 y otra vez 500
 
 #13
 b = 500
@@ -111,6 +117,7 @@ print(b)
 b=foobar()
 print(b)
 
+#imprimira 500 2 veces, luego 300 2 veces
 
 #14
 def foo():
@@ -121,6 +128,7 @@ def bar():
     print(3)
 foo()
 
+#imprimira 1,3 y 2 en ese orden
 
 #15
 def foo():
@@ -133,3 +141,5 @@ def bar():
     return 5
 y = foo()
 print(y)
+
+#imprimira 1,3 luego retonara 5, despues imprimira 3 y 5, y luego retonara 10
