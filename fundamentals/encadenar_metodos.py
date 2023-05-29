f5168f700ac922f23d115acf690f3039640c590c
Camilo Flores <camilocifz36@gmail.com>
2023-05-29 16:59:34 -0400

    encadenar_metodos

--- fundamentals/encadenar_metodos.py
+++ fundamentals/encadenar_metodos.py
@@ -6,13 +6,16 @@ class Usuario:
     def hacer_deposito(self, cantidad):
         self.balance_cuenta += cantidad
         print(f"Se realizo un deposito de {cantidad}.")
+        return self
 
     def hacer_retiro(self, cantidad):
         if self.balance_cuenta >= cantidad:
             self.balance_cuenta -= cantidad
             print(f"Se realizo un retiro de {cantidad}.")
+            return self
         else:
             print("Saldo insuficiente. No se pudo realizar el retiro.")
+            return self
 
     def mostrar_balance_usuario(self):
         print(f"Nombre: {self.nombre} {self.apellido}")
@@ -23,6 +26,7 @@ class Usuario:
             self.balance_cuenta -= cantidad
             usuario2.balance_cuenta += cantidad
             print(f"Se transfirieron {cantidad} de {self.nombre} {self.apellido} a {usuario2.nombre} {usuario2.apellido}.")
+            return self
         else:
             print("Saldo insuficiente. No se pudo realizar la transferencia.")
 
@@ -31,24 +35,14 @@ user1=Usuario("Claudio", "Contreras", 300)
 user2=Usuario("Lisa", "Simpson", 150)
 user3=Usuario("Luis", "Perez", 500)
 
-user1.hacer_deposito(100)
-user1.hacer_deposito(200)
-user1.hacer_deposito(150)
-user1.hacer_retiro(300)
-user1.mostrar_balance_usuario()
+user1.hacer_deposito(100).hacer_deposito(200).hacer_deposito(150).hacer_retiro(300).mostrar_balance_usuario()
 
-user2.hacer_deposito(200)
-user2.hacer_deposito(120)
-user2.hacer_retiro(90)
-user2.hacer_retiro(110)
-user2.mostrar_balance_usuario()
 
-user3.hacer_deposito(140)
-user3.hacer_retiro(30)
-user3.hacer_retiro(90)
-user3.hacer_retiro(110)
-user3.mostrar_balance_usuario()
+user2.hacer_deposito(200).hacer_deposito(120).hacer_retiro(90).hacer_retiro(110).mostrar_balance_usuario()
 
-user1.transfer_dinero(user3,100)
-user1.mostrar_balance_usuario()
+user3.hacer_deposito(140).hacer_retiro(30).hacer_retiro(90).hacer_retiro(110).mostrar_balance_usuario()
+
+
+user1.transfer_dinero(user3,100).mostrar_balance_usuario
+
 user3.mostrar_balance_usuario()
